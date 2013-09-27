a803260010bd22edbc3d3948b5a7555707bb5cb1
Iskren Slavov <iskren.s@gmail.com>
2013-09-27 16:16:21 +0300

    A small DateTime check added.

--- maillogimporter/writer/mongo.php
+++ maillogimporter/writer/mongo.php
@@ -29,11 +29,15 @@ class Mongo implements WriterInterface
     {
         // Get the last item
         $lastItem = $this->_db->find()->sort(array('date_updated' => -1))->limit(1)->getNext();
-        if (empty($lastItem)) {
+        if (empty($lastItem) || empty($lastItem['date_updated'])) {
             return null;
         }
-        // Convert item's 'date_updated' to DateTime
+        // Extract 'date_updated' column only
         $dbDateTime = $lastItem['date_updated'];
+        if (empty($dbDateTime['date']) || empty($dbDateTime['timezone'])) {
+            return null;
+        }
+        // Convert 'date_updated' to DateTime
         return new \DateTime($dbDateTime['date'], new \DateTimeZone($dbDateTime['timezone']));
     }
 
